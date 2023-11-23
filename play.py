0cbf786c2069a6c2cf3c2387a57ea41f8498908e
mohammad <147561435+mohammadhoseeinnemati@users.noreply.github.com>
2023-11-23 00:45:37 -0800

    Update play.py

--- play.py
+++ play.py
@@ -6,22 +6,7 @@ print("absurd")
 print("0 = Left ")
 print("1 = Right")
 
-
-user = input("plz enter two usersn or single user: ")
-
-if user ==  "two usersn":
-    player_1 = input("plz enter your name player_1:")
-    player_2 = input("plz enter your name player_2:")
-
-    player_1 = int(input("plz enter Flowers At Side Left or Right player_1: "))
-    player_2 = int(input("Which  Hand gol Is player_2:"))
-if player_1 == player_2:
-    print(f"you lost player_1")
-elif player_1 !=player_2:
-    print(f"you win player_1")
-
-if user == "single user":
-    plaayer =int(input("plz enter Flowers At Side Left or Right: "))
+plaayer =int(input("plz enter Flowers At Side Left or Right: "))
 
 import random
 rnd = (random .randint(0,1))
@@ -40,15 +25,6 @@ if rnd == plaayer1:
         print("you win")
 elif rnd!=plaayer1:
     print("you lost")
-
-
-
-
-
-
-
-        
-
 
 
 
