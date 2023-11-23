f57e7d21508b72c00f0b2353def9663db10edb12
mohammad <147561435+mohammadhoseeinnemati@users.noreply.github.com>
2023-11-23 00:42:40 -0800

    Update play2.py

--- play2.py
+++ play2.py
@@ -1,29 +1,35 @@
-
-print("rock")
-print("paper")
-print("scissor")
-
-player1 = int(input("player1 plz enter yuor moove:"))
-
 import random
-rnd = (random .randint(0,2))
-print(rnd)
+print("rock ...")
+print("paper ...")
+print("scissors ...")
 
-if player1 == "0" and rnd == "1":
-    print("you lost")
-elif player1 == "0" and rnd == "2":
-    print("you win")
-elif player1 == "0" and rnd =="0":
-    print("equal")
-elif player1 == "1" and rnd == "0":
-    print("you win")
-elif player1 == "1" and rnd == "2":
-    print("you lost")
-elif player1 == "1" and rnd =="1":
-    print("equal")
-elif player1 == "2" and rnd == "0":
-    print("you lost")
-elif player1 == "2" and rnd == "1":
-    print("you win")
-elif player1 == "2" and rnd =="2":
-    print("equal")
+randomNumber = random.randint(0,2)
+computerMove = "rock"
+
+if randomNumber == 0:
+    computerMove = "rock"
+elif randomNumber == 1:
+    computerMove = "paper"
+elif randomNumber == 2:
+    computerMove = "scissors"
+
+player1 = input("player1 , make your move : ")
+print(f"player2 , make your move : {computerMove}")
+player2 = computerMove
+
+if player1 == "rock" and player2 == "scissors":
+    print ("player 1 wins !...")
+elif player1 == "rock" and player2 == "paper":
+    print("player 2 wins !...")
+elif player1 == "paper" and player2 == "rock":
+    print("player 1 wins !...")
+elif player1 == "paper" and player2 == "scissors":
+    print("player 2 wins !...")
+elif player1 == "scissors" and player2 == "paper":
+    print("player1 wins !...")
+elif player1 == "scissors" and player2 == "rock":
+    print("player2 wins !...")
+elif player1 == player2:
+    print ("thats a tie !...")
+else:
+    print("something went wrong !..")
